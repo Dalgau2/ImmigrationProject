bb91e8b44a0ccdbc98fb438a3081504ca7e370c9
gautamProject <gmgautam2374@gmail.com>
2024-04-19 22:39:45 +0530

    cheking for git push ins working or not

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LayoutOfImmigration />}>
+          {/* hello chk gor commit */}
           <Route index element={<HomePageForImmigration/>} />
         </Route>
       </Routes>
